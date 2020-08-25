dd97b12233f4d4f8373ffd8150eba9fc7e0af8a4
MatthewThabo <65297029+MatthewThabo@users.noreply.github.com>
2020-08-25 14:22:27 +0200

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+    $DB_NAME = "Camagru";
+    $DB_DSN = "mysql:host=localhost;dbname=".$DB_NAME;
+    $DB_HOST = "localhost";
+    $DB_USER = "root";
+    $DB_PASSWORD = "projects";
+    try{
+        $conn = new PDO("mysql:host=localhost;dbname=".$DB_NAME, $DB_USER , $DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        // echo "Error: ". $e->getMessage();
+    }
+?>
---
+++ js/upload.js
@@ -0,0 +1,25 @@
+function addSup(obj) {
+    var imagescr = obj.src;
+    var imgsup = document.getElementById('superimg');
+    imgsup.setAttribute('src', imagescr);
+    document.getElementById('capture2').disabled = false;
+}
+
+(function() {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        flow = document.getElementById('superimg'),
+        image = document.getElementById('img_upload');
+
+
+    document.getElementById('capture2').addEventListener('click', function () {
+        context.drawImage(image, 0, 0, 540, 380);
+        context.drawImage(flow, 0, 0, 540, 380);
+        var element = document.getElementById('picture');
+        var img = canvas.toDataURL('image/jpeg');
+        element.value = img;
+        alert(img);
+        document.getElementById('capture-form').submit();
+    });
+
+}) ();
